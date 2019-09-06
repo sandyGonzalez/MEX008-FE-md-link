c7b397ddbede9f740c5bff4a74913ac40af7829e
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-09-06 14:28:49 -0500

    transformando la funcion que busca los links a una promesa para poder manipular el resultado

--- modules/getLinks.js
+++ modules/getLinks.js
@@ -1,21 +1,23 @@
 const path = require("path");
 
- const searchLinks =(typeFile)=> {
-  const regex = /\[(.+)\]\((https?.+)\)/gm;
-  let match;
-  let i = 0;
-  let newArray = [];
-  while((match = regex.exec(typeFile)) !== null) {
-    const newObject = {
-      href: match[2],
-      text: match[1],
-      File:path.resolve()
-    };
-    i++;
-    newArray.push(newObject);
+const searchLinks = (typeFile) => {
+    return new Promise((resolve) => {
+      const regex = /\[(.+)\]\((https?.+)\)/gm;
+      let match;
+      let i = 0;
+      let newArray = [];
+      while ((match = regex.exec(typeFile)) !== null) {
+        const newObject = {
+          href: match[2],
+          text: match[1],
+          File: path.resolve()
+        };
+        i++;
+        newArray.push(newObject);
+      }
+      resolve(newArray);
+    })
   }
-  return newArray;
-};
 
- module.exports = searchLinks;
- 
+
+    module.exports = searchLinks;
