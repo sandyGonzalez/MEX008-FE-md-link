2912c93964c441afca591d5cb24f3574bd1d16fd
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-09-06 18:29:46 -0500

    Haciendo test de la funcion que lee el archivo

--- modules/getFile.js
+++ modules/getFile.js
@@ -1,7 +1,18 @@
 const path = require("path");
 
 const getFileMd = (inputFile) => {
-   return path.extname(inputFile) === ".md";
- };
+
+  return path.extname(inputFile) === ".md";
+};
 
 module.exports = getFileMd;
+
+
+
+
+
+// const getFileMd = (inputFile) => {
+//   return path.extname(inputFile) === ".md";
+// };
+
+// module.exports = getFileMd;
